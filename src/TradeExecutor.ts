11ea530da7651eb072998d3814cd782a119acfb6
Tarcisio Ads <tarcisioasd@gmail.com>
2025-11-22 20:41:19 -0300

    fix: dig mistake

--- src/TradeExecutor.ts
+++ src/TradeExecutor.ts
@@ -140,7 +140,7 @@ export class TradeExecutor {
         message: 'Trade executed successfully',
         data: {
           ...executionResult,
-          volumeMarginAdded: volumeMarginInfo
+          volumeMarginAdded: volumeMarginInfo,
           sentimentMarginAdded: sentimentMarginInfo
         }
       };
